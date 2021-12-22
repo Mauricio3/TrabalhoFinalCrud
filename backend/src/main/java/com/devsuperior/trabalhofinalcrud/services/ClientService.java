426f689efe23b8e8d42eadc1b612ee7de0a77552
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2021-12-22 17:33:40 -0300

    Client Paged, Find by Id, new Client

--- backend/src/main/java/com/devsuperior/trabalhofinalcrud/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/trabalhofinalcrud/services/ClientService.java
@@ -1,15 +1,17 @@
 package com.devsuperior.trabalhofinalcrud.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.trabalhofinalcrud.dto.ClientDTO;
 import com.devsuperior.trabalhofinalcrud.entities.Client;
 import com.devsuperior.trabalhofinalcrud.repositories.ClientRepository;
+import com.devsuperior.trabalhofinalcrud.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -18,8 +20,28 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow( () -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);	
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setIncome(dto.getIncome());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/trabalhofinalcrud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.trabalhofinalcrud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
